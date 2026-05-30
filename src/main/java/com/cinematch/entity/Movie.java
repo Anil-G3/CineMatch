5fa533056a5814791557ca1fafce0ac3f08bfcb6
Anil2030at <anil2004333@gmail.com>
2026-05-30 13:40:04 +0530

    Fixed Lazy Fetch

--- src/main/java/com/cinematch/entity/Movie.java
+++ src/main/java/com/cinematch/entity/Movie.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -55,7 +56,7 @@ public class Movie {
 	@Builder.Default
 	private Integer totalRatings = 0;
 	
-	@ManyToMany
+	@ManyToMany(fetch=FetchType.EAGER)
 	@JoinTable(
 	    name = "movie_genres",
 	    joinColumns = @JoinColumn(name = "movie_id"),
