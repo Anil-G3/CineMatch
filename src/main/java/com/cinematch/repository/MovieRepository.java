f4fd310fb8c58057d7f0801fbb90c8d371348def
Anil-G3 <anilkumar.th20@gmail.com>
2026-06-22 10:55:19 +0530

    Update MovieRepository.java

--- src/main/java/com/cinematch/repository/MovieRepository.java
+++ src/main/java/com/cinematch/repository/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.cinematch.repository;
 
+import org.springframework.data.repository.query.Param;
 import com.cinematch.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
